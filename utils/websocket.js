44c5f5146298680ac57389fe75f4b22b668eeb7e
Chiran Jeewantha <67968706+Chikuu98@users.noreply.github.com>
2024-12-22 21:14:16 +0530

    glitch socket done (#14)

--- utils/websocket.js
+++ utils/websocket.js
@@ -3,29 +3,48 @@ const WebSocket = require("ws");
 let ws;
 
 const connectToWebSocket = () => {
-  const WS_URL = 'wss://sheetbookingsocket.glitch.me'; // Replace with your Glitch WebSocket URL
-  ws = new WebSocket(WS_URL);
+  const wsUrl = process.env.WS_URL;
+  if (!wsUrl) {
+    console.error("WebSocket URL is not defined");
+    return;
+  }
 
-  ws.on('open', () => {
-    console.log('Connected to Glitch WebSocket server');
+  ws = new WebSocket(wsUrl, {
+    headers: {
+      "user-agent": "API CLIENT",
+    },
   });
 
-  ws.on('error', (error) => {
-    console.error('WebSocket error:', error);
+  ws.on("open", () => {
+    console.log("Connected to WebSocket server");
   });
 
-  ws.on('close', () => {
-    console.log('Disconnected from WebSocket server');
+  ws.on("error", (error) => {
+    console.error("WebSocket error:", error);
+  });
+
+  ws.on("close", () => {
+    console.log("WebSocket connection closed");
   });
 };
 
-// Create a function to broadcast data to Glitch WebSocket server
 const broadcast = (data) => {
-  if (ws && ws.readyState === WebSocket.OPEN) {
-    ws.send(JSON.stringify(data));
+  if (!ws || ws.readyState !== WebSocket.OPEN) {
+    console.log("WebSocket is closed. Reconnecting...");
+    connectToWebSocket();
+
+    setTimeout(() => {
+      if (ws && ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify(data));
+      } else {
+        console.log("Still unable to connect, please try again later.");
+      }
+    }, 3000);
   } else {
-    console.error("WebSocket connection is not open");
+    ws.send(JSON.stringify(data));
   }
 };
+
+connectToWebSocket();
 
 module.exports = { connectToWebSocket, broadcast };
