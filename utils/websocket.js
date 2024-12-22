078b100805102991d64194be2abbaa6bb82ae4ab
Chiran Jeewantha <67968706+Chikuu98@users.noreply.github.com>
2024-12-22 19:13:27 +0530

    glitch testing one

--- utils/websocket.js
+++ utils/websocket.js
@@ -1,12 +1,31 @@
-const { getWebSocketServer } = require("../websocket");
+const WebSocket = require("ws");
 
-const broadcast = (data) => {
-  const wss = getWebSocketServer();
-  wss.clients.forEach((client) => {
-    if (client.readyState === 1) {
-      client.send(JSON.stringify(data));
-    }
+let ws;
+
+const connectToWebSocket = () => {
+  const WS_URL = 'wss://sheetbookingsocket.glitch.me'; // Replace with your Glitch WebSocket URL
+  ws = new WebSocket(WS_URL);
+
+  ws.on('open', () => {
+    console.log('Connected to Glitch WebSocket server');
+  });
+
+  ws.on('error', (error) => {
+    console.error('WebSocket error:', error);
+  });
+
+  ws.on('close', () => {
+    console.log('Disconnected from WebSocket server');
   });
 };
 
-module.exports = { broadcast };
+// Create a function to broadcast data to Glitch WebSocket server
+const broadcast = (data) => {
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    ws.send(JSON.stringify(data));
+  } else {
+    console.error("WebSocket connection is not open");
+  }
+};
+
+module.exports = { connectToWebSocket, broadcast };
